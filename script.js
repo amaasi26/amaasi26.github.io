cf042baf6d931c0c172a96f4c79e3ede8b3889ee
amaasi26 <112572461+amaasi26@users.noreply.github.com>
2022-09-20 10:51:42 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const navLinks = document.querySelectorAll(".nav-menu")
+
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 })
 
-navMenu.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-menu');
-    })
-})
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
